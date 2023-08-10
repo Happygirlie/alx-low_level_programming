a859750f7c5cabac22e511061eb34a50a36ca7ad
Happygirlie <happinesschinweopara@gmail.com>
2023-08-10 02:20:41 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a new line with printf function
+ * Return: It should return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
